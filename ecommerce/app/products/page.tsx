0082cb77c578583142f056f946f37e7e45a71bd2
Katieum <eaglenyoni@gmail.com>
2025-11-25 18:12:00 +0200

    Making the heading to be a link

--- ecommerce/app/products/page.tsx
+++ ecommerce/app/products/page.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import { products } from "../product-data";
 import ProductsList from "../ProductsList";
 
 export default function ProductsPage() {
   return (
     <>
-      <h1 className="font-bold text-3xl text-cyan-600 justify-self-center">
+      <Link href={"/"}>
+        <h1 className="font-bold text-3xl text-cyan-600 justify-self-center">
         Makhulu Store
       </h1>
+      </Link>
       <ProductsList products={products} />
     </>
   );
