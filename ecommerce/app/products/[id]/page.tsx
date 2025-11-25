004f2c5a50ed89db101c23c25083ad49ca3a3a6a
Katieum <eaglenyoni@gmail.com>
2025-11-25 17:44:01 +0200

    Setting boilerplate for the page

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default function ProductDetailsPage() {
+import { products } from "@/app/product-data";
+
+export default function ProductDetailPage({ params }: { params: { id: string }})
+{
+  const product = products.find(p => p.id === params.id);
+
   return (
     <>
-      <h1>Hello, World!</h1>
+      <h1>{product!.name}</h1>
     </>
   );
 }
