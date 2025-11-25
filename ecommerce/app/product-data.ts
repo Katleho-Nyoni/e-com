786c769356af7ad4a64e8d395f6090d7d0e7d75c
Katieum <eaglenyoni@gmail.com>
2025-11-25 17:43:03 +0200

    swapping a phone for gaming chair

--- ecommerce/app/product-data.ts
+++ ecommerce/app/product-data.ts
@@ -11,23 +11,23 @@ export const products: Product [] = [{
     name: "Seanamarena Blanket",
     imageURL: "blanket.jpeg",
     description: "The well celebrated Seanamarena-Poone blanket",
-    price: 29.99
+    price: 799.99
 },{
     id: "101",
     name: "Asus VivoBook Go",
     imageURL: "laptop.webp",
     description: "A high-performance laptop for all your needs",
-    price: 34.99
+    price: 7549.99
 },{
     id: "102",
     name: "Black Cast Iron Pots",
     imageURL: "pots.webp",
     description: "A set of ceramic pots for your home cooked meals",
-    price: 19.99
+    price: 599.99
 },{
     id: "103",
-    name: "Apple iPhone 16 Pro",
-    imageURL: "phone.webp",
-    description: "The latest smartphone with cutting-edge features",
-    price: 39.99
+    name: "Gaming Chair",
+    imageURL: "gaming-chair.webp",
+    description: "A comfortable and ergonomic gaming chair for long hours of play",
+    price: 6499.99
 }];
