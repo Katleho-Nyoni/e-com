b1e47df608ee60e4b31351cb6d0ca7fedd2ad721
Katieum <eaglenyoni@gmail.com>
2025-11-26 00:25:04 +0200

    Adding styles and title page

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { products } from "@/app/product-data";
 
 export default async function ProductDetailPage({
@@ -9,7 +10,11 @@ export default async function ProductDetailPage({
 
   return (
     <>
+      <h2 className="font-bold text-2xl mb-6"> Product Details</h2>
+      <Image src={"/" + product!.imageURL} alt={product!.name} width={200} height={200} />
       <h1 className="text-white font-bold">{product!.name}</h1>
+      <p>R{product!.price}</p>
+      <p>R{product!.description}</p>
     </>
   );
 }
